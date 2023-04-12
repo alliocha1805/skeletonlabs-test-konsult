4407affb66459238abc286051fadfaba4af16b3d
alliocha1805 <alexis.piloy@gmail.com>
2023-04-12 18:09:32 +0200

    1 file modified

--- src/routes/client/+page.svelte
+++ src/routes/client/+page.svelte
@@ -1,5 +1,34 @@
 <script lang="ts">
+  import Grid from "gridjs-svelte"
+	import { frFR } from "gridjs/l10n";
+	import { onMount, afterUpdate } from 'svelte';
 
+  const data = [
+    { name: "John", email: "john@example.com" },
+    { name: "Mark", email: "mark@gmail.com" },
+		{ name: "Mark", email: "mark@gmail.com" },
+		{ name: "Mark", email: "mark@gmail.com" },
+		{ name: "Mark", email: "mark@gmail.com" },
+		{ name: "Mark", email: "mark@gmail.com" },
+		{ name: "Mark", email: "mark@gmail.com" },
+		{ name: "Mark", email: "mark@gmail.com" },
+		{ name: "Mark", email: "mark@gmail.com" },
+		{ name: "Mark", email: "mark@gmail.com" },
+		{ name: "Mark", email: "mark@gmail.com" },
+		{ name: "Mark", email: "mark@gmail.com" },
+		{ name: "Mark", email: "mark@gmail.com" },
+		{ name: "Mark", email: "mark@gmail.com" },
+  ]
+
+	const className = {
+		table: 'table',
+		paginationButton: 'btn variant-filled',
+		paginationButtonCurrent: ' ml-2 mr-2',
+		input:'input',
+		pagination: 'flex flex-col justify-end',
+		header:"text-black"
+	};
+	const language = frFR;
 </script>
 
 <svelte:head>
@@ -9,6 +38,11 @@
 
 <div class="p-10 space-4">
   <h1 class="pb-5">Liste des Clients</h1>
-
-
+	<div id=my-grid-table>
+		<Grid data={data} {className} language={language} search pagination={{ enabled: true, limit: 5}}/>
+	</div>
 </div>
+
+<style>
+
+</style>
