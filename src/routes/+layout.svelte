6bd7a4486220fab036649a3279205625e199e613
alliocha1805 <alexis.piloy@gmail.com>
2023-05-22 14:52:06 +0200

    4 files modified

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,11 +9,23 @@
 	import Navigation from '$lib/Navigation/Navigation.svelte';
 	import { Drawer, drawerStore } from '@skeletonlabs/skeleton';
 	import { Avatar } from '@skeletonlabs/skeleton';
-
+	import { popup } from '@skeletonlabs/skeleton';
+	import type { PopupSettings } from '@skeletonlabs/skeleton';
+	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
 	function drawerOpen() 
   {
 		drawerStore.open({});
 	}
+	const popupMenu: PopupSettings = {
+	// Represents the type of event that opens/closed the popup
+	event: 'click',
+	// Matches the data-popup value on your popup element
+	target: 'popupMenu',
+	// Defines which side of your trigger the popup will appear
+	placement: 'bottom',
+};
 </script>
 
 <!-- App Shell -->
@@ -39,10 +51,28 @@
 			<strong class="text-xl uppercase">Konsult.io</strong>
 			
 			<svelte:fragment slot="trail">
-			<Avatar
-				border="border-4 border-surface-300-600-token hover:!border-primary-500"
-				cursor="cursor-pointer"
-				initials="AP"/>
+			<div class="">
+				<button type="button" class="btn-icon btn-icon-xl variant-primary" use:popup={popupMenu}><svg class="h-10 w-10 text-white"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M7 10h3v-3l-3.5 -3.5a6 6 0 0 1 8 8l6 6a2 2 0 0 1 -3 3l-6-6a6 6 0 0 1 -8 -8l3.5 3.5" /></svg></button>
+				<div class="card p-4 shadow-xl" data-popup="popupMenu">
+				<div class="btn-group-vertical">
+					<button type="button" class="btn variant-filled-secondary mb-2">
+						<span>
+							<svg class="h-6 w-6 text-white"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="12" cy="7" r="4" />  <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /></svg>
+						</span>
+						<span>Mon compte</span>
+					</button>
+					<button type="button" class="btn variant-filled-secondary mb-2">
+						<span><svg class="h-6 w-6 text-white"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"/>
+						</svg>
+						</span>
+						<span>Deconnexion</span>
+					</button>
+				</div>
+
+				<div class="arrow bg-surface-100-800-token" />
+				</div>
+			</div>
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
