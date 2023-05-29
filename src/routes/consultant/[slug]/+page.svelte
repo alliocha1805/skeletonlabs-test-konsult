51549879e0b0f89b5444593923fdbaee6bb2972e
alliocha1805 <alexis.piloy@gmail.com>
2023-05-29 13:53:05 +0200

    1 file modified

--- src/routes/consultant/[slug]/+page.svelte
+++ src/routes/consultant/[slug]/+page.svelte
@@ -2,7 +2,25 @@
   import { page } from '$app/stores'
 	import { Avatar } from '@skeletonlabs/skeleton';
 	import { TabGroup, Tab } from '@skeletonlabs/skeleton';
+	import { InputChip } from '@skeletonlabs/skeleton';
+	import { Autocomplete } from '@skeletonlabs/skeleton';
+	import type { AutocompleteOption } from '@skeletonlabs/skeleton';
 	let tabSet: number = 0;
+	let inputChip = '';
+	let inputChipList: string[] = ['vanilla', 'chocolate'];
+
+	const flavorOptions: AutocompleteOption[] = [
+		{ label: 'Vanilla', value: 'vanilla', keywords: 'plain, basic', meta: { healthy: false } },
+		{ label: 'Chocolate', value: 'chocolate', keywords: 'dark, white', meta: { healthy: false } },
+		{ label: 'Strawberry', value: 'strawberry', keywords: 'fruit', meta: { healthy: true } },
+		{ label: 'Neapolitan', value: 'neapolitan', keywords: 'mix, strawberry, chocolate, vanilla', meta: { healthy: false } },
+		{ label: 'Pineapple', value: 'pineapple', keywords: 'fruit', meta: { healthy: true } },
+		{ label: 'Peach', value: 'peach', keywords: 'fruit', meta: { healthy: true } }
+	];
+
+function onInputChipSelect(event: any): void {
+	inputDemo = event.detail.label;
+}
 </script>
 
 <svelte:head>
@@ -11,6 +29,7 @@
 </svelte:head>
 
 <div class="p-10 space-4 grid grid-cols-1 gap-4 md:grid grid-cols-4 gap-4">
+		<!--Bloc en tete information-->
 		<div class="card p-4 col-span-4 bg-variant-filled-surface flex flex-row" style="justify-content:space-between;">
 			<Avatar src="https://dnfx0kvkzsynw.cloudfront.net/websites/25/personnages/arthur.jpg" width="w-32" class="ml-10 mr-10 hidden none md:block"/>
 			<div class="flex flex-col justify-center">
@@ -24,22 +43,49 @@
 				</a>
 			</div>
 		</div>
-		<div class="card p-4 col-span-4 bg-variant-filled-surface flex flex-col">
-			<h3 class= "text-center">Informations</h3>
+		<!--Bloc col cote gauche-->
+		<div class="flex flex-col col-span-4 md:col-span-1">
+			<div class="card p-4 mb-4 bg-variant-filled-surface flex flex-col">
+				<h3 class= "text-center">Informations</h3>
+				<InputChip bind:input={inputChip} bind:value={inputChipList} name="chips" />
+				<div class="card w-full max-w-sm max-h-48 p-4 overflow-y-auto">
+					<Autocomplete
+						bind:input={inputChip}
+						options={flavorOptions}
+						denylist={inputChipList}
+						on:selection={onInputChipSelect}
+					/>
+				</div>
+			</div>
+			<div class="card p-4 bg-variant-filled-surface flex flex-col" style="justify-content:space-between;">
+				<h3 class= "text-center">Skills</h3>
+			</div>		
 		</div>
-		<div class="card p-4 col-span-1 bg-variant-filled-surface flex flex-col" style="justify-content:space-between;">
-			<h3>Skills</h3>
-		</div>
-		<div class="card p-4 col-span-3 bg-variant-filled-surface c" style="justify-content:space-between;">
-			<h3>Expériences</h3>
+		<!--Bloc Experiences-->
+		<div class="card p-4 col-span-4 bg-variant-filled-surface md:col-span-3" style="justify-content:space-between;">
+			<h3 class= "text-center">Expériences</h3>
 			<div class="grid grid-cols-4">
 				<div class="col-span-1">
 					<Avatar src="https://www.francebleu.fr/s3/cruiser-production/2021/05/34c62b6c-b075-4b33-bbc4-1a92dedf2d77/1200x680_img_6957.jpg" width="w-32" class="ml-10 mt-10 hidden none md:block"/>
 				</div>
-				<div class="col-span-3">
+				<div class="col-span-4 md:col-span-3">
 					<div class="flex flex-col">
 						<h4>Roi de Bretagne</h4>
 						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus consequat, ante a sodales sagittis, nunc urna laoreet risus, eget pulvinar turpis dui sit amet augue. Vivamus ac volutpat nunc. Aliquam imperdiet id nulla non eleifend. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque in sagitstis erat, a accumsan lectus. Curabitur ante dolor, ultricies nec mollis ut, elementum at ipsum. Suspendisse ac nisi ut est scelerisque semper. Donec sed felis pretium, fringilla nisl sit amet, vulputate eros. Vestibulum dolor justo, rhoncus vel posuere at, mattis quis velit. Duis efficitur eleifend lectus a tristique. Donec nec venenatis elit.</p>
+						<i>Jan 500 - Dec 535</i>
+						<a href="/" class="btn variant-filled m-4">Detail</a>
+					</div>
+				</div>
+				<hr class="col-span-4 mt-4 mb-4"/>
+			</div>
+			<div class="grid grid-cols-4">
+				<div class="col-span-1">
+					<Avatar src="https://www.francebleu.fr/s3/cruiser-production/2021/05/34c62b6c-b075-4b33-bbc4-1a92dedf2d77/1200x680_img_6957.jpg" width="w-32" class="ml-10 mt-10 hidden none md:block"/>
+				</div>
+				<div class="col-span-4 md:col-span-3">
+					<div class="flex flex-col">
+						<h4>Roi de Bretagne</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus consequat, ante a sodales sagittis, nunc urna laoreet risus, eget pulvinar turpis dui sit amet augue. Vivamus ac volutpat nunc. Aliquam imperdiet id nulla non eleifend. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesquse in sagittis erat, a accumsan lectus. Curabitur ante dolor, ultricies nec mollis ut, elementum at ipsum. Suspendisse ac nisi ut est scelerisque semper. Donec sed felis pretium, fringilla nisl sit amet, vulputate eros. Vestibulum dolor justo, rhoncus vel posuere at, mattis quis velit. Duis efficitur eleifend lectus a tristique. Donec nec venenatis elit.</p>
 						<i>Jan 500 - Dec 535</i>
 						<a href="/" class="btn variant-filled">Detail</a>
 					</div>
@@ -50,7 +96,7 @@
 				<div class="col-span-1">
 					<Avatar src="https://www.francebleu.fr/s3/cruiser-production/2021/05/34c62b6c-b075-4b33-bbc4-1a92dedf2d77/1200x680_img_6957.jpg" width="w-32" class="ml-10 mt-10 hidden none md:block"/>
 				</div>
-				<div class="col-span-3">
+				<div class="col-span-4 md:col-span-3">
 					<div class="flex flex-col">
 						<h4>Roi de Bretagne</h4>
 						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus consequat, ante a sodales sagittis, nunc urna laoreet risus, eget pulvinar turpis dui sit amet augue. Vivamus ac volutpat nunc. Aliquam imperdiet id nulla non eleifend. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesquse in sagittis erat, a accumsan lectus. Curabitur ante dolor, ultricies nec mollis ut, elementum at ipsum. Suspendisse ac nisi ut est scelerisque semper. Donec sed felis pretium, fringilla nisl sit amet, vulputate eros. Vestibulum dolor justo, rhoncus vel posuere at, mattis quis velit. Duis efficitur eleifend lectus a tristique. Donec nec venenatis elit.</p>
@@ -58,7 +104,21 @@
 						<a href="/" class="btn variant-filled">Detail</a>
 					</div>
 				</div>
-				<hr class="col-span-4 mt-4"/>
+				<hr class="col-span-4 mt-4 mb-4"/>
+			</div>
+			<div class="grid grid-cols-4">
+				<div class="col-span-1">
+					<Avatar src="https://www.francebleu.fr/s3/cruiser-production/2021/05/34c62b6c-b075-4b33-bbc4-1a92dedf2d77/1200x680_img_6957.jpg" width="w-32" class="ml-10 mt-10 hidden none md:block"/>
+				</div>
+				<div class="col-span-4 md:col-span-3">
+					<div class="flex flex-col">
+						<h4>Roi de Bretagne</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus consequat, ante a sodales sagittis, nunc urna laoreet risus, eget pulvinar turpis dui sit amet augue. Vivamus ac volutpat nunc. Aliquam imperdiet id nulla non eleifend. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesquse in sagittis erat, a accumsan lectus. Curabitur ante dolor, ultricies nec mollis ut, elementum at ipsum. Suspendisse ac nisi ut est scelerisque semper. Donec sed felis pretium, fringilla nisl sit amet, vulputate eros. Vestibulum dolor justo, rhoncus vel posuere at, mattis quis velit. Duis efficitur eleifend lectus a tristique. Donec nec venenatis elit.</p>
+						<i>Jan 500 - Dec 535</i>
+						<a href="/" class="btn variant-filled">Detail</a>
+					</div>
+				</div>
+				<hr class="col-span-4 mt-4 mb-4"/>
 			</div>
 		</div>
 </div>
