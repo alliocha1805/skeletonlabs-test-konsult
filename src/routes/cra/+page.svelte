26dc24c9da45c761fc7ea1f11feb773a29dbcdf9
alliocha1805 <alexis.piloy@gmail.com>
2023-05-31 17:19:17 +0200

    4 files modified, 1 added

--- src/routes/cra/+page.svelte
+++ src/routes/cra/+page.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
-	import { RevoGrid } from "@revolist/svelte-datagrid";
+	import Tabulator from '$lib/Tabulator/Tabulator.svelte';
+	let donnees={};
+	let colonne={};
 </script>
+
 <svelte:head>
 	<title>Comptes Rendus D'activité</title>
 	<meta name="description" content="Comptes rendus d'activité" />
+	<link href="https://unpkg.com/tabulator-tables@4.9.1/dist/css/tabulator.min.css" rel="stylesheet">
 </svelte:head>
 
 <!--Une idée serait d'avoir deux "picker" : MOIS et ANNEE ou un Range picker de date. A partir de la on affichera une Grid custom avec le plugin https://revolist.github.io/revogrid/. Ce plugin peut stocker en fichier/blob ou string-->
@@ -15,5 +19,6 @@
 	</div>
 	<div class="card p-4 mb-4 bg-variant-filled-surface flex flex-col col-span-4 xl:col-span-3">
 		<h3>Feuille de temps</h3>
+		<Tabulator data={donnees} autoColumns=true/>
 	</div>
 </div>
